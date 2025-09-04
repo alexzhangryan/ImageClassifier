66d9a92bb05c68d8f28ef92e71ffef069b001ecc
alexzhangryan <alexzhangryan@gmail.com>
2025-09-03 19:45:20 -0500

    Initial commit (without large files)

---
+++ main.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+print(torch.version.cuda)
+print(torch.cuda.is_available())
+
+
+class PlayingCardDataset(Dataset):
+    def __init__(self, data_dir, transform = None):
+        self.data = ImageFolder(data_dir, transform = transform)
+        
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self, idx):
+        return self.data[idx]
+    
+    @property
+    def classes(self):
+        return self.data.classes
+    
+train_folder='archive/train'
+valid_folder='archive/valid'
+test_folder='archive/test'
+
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+train_dataset = PlayingCardDataset(train_folder, transform)
+val_dataset = PlayingCardDataset(valid_folder, transform)
+test_dataset = PlayingCardDataset(test_folder, transform)
+
+train_loader = DataLoader(train_dataset, batch_size = 32, shuffle = True)
+val_loader = DataLoader(val_dataset, batch_size = 32, shuffle = False)
+testloader = DataLoader(test_dataset, batch_size = 32, shuffle = False)
+
+class SimpleCardClassifer(nn.Module):
+    def __init__(self, num_classes = 53):
+        super(SimpleCardClassifer, self).__init__()
+        #define parts of model
+        self.base_model = timm.create_model('efficientnet_b0', pretrained = True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+        enet_out_size = 1280
+        #classifier
+        self.classifier = nn.Linear(enet_out_size, num_classes)
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+    
+model = SimpleCardClassifer(num_classes = 53)
+
+#loss functioon
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr = 0.001)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+num_epoch = 5
+train_losses, val_losses = [], []
+
+model = SimpleCardClassifer(num_classes = 53)
+model.too(device)
+
+for epoch in range(num_epoch):
+    #set model to train
+    model.train()
+    running_loss = 0.0
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * images.size(0)
+    train_loss = running_loss / len(train_loader.dataset)
+    train_losses.append(train_loss)
+
+    #validation
+
+    model.eval()
+    running_loss = 0.0
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * images.size(0)
+    val_loss = running_loss / len(val_loader.dataset)
+    val_losses.append(val_loss)
+
+    #print epoch stats
+    print(f"Epoch {epoch+1}/{num_epoch} - Train loss: {train_loss}, Validation loss: {val_loss}")
